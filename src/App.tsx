732e8ea1254dc0aefc9f2619d31296c89c5791a3
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-12-26 09:03:39 -0300

    commit 1

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="cartao_container">
+        <div className="cartao_frente"></div>
+        <div className="cartao_tras"></div>
+      </div>
+      <div className="formulario_container">
+        <div className="formulario_container__formulario">
+          <div className="formulario"></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
